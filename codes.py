c9df66a8ca88f1070a0392108ea13c9d28918da8
Jacek <jk@jk-mac.local>
2019-11-14 17:54:17 +0000

    add licence and comments

--- codes.py
+++ codes.py
@@ -1,10 +1,43 @@
+# Failproof code generator
+# Copyright (C) 2019 Jacek Kowalski
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import Levenshtein as l
 from random import randint
 from pprint import pprint
 import unittest
-
+import string
+import argparse
+from math import floor
 class CodeGenerator():
-    def __init__(self, code_len, min_dist, max_codes_cnt):
+    def __init__(self, code_len, min_dist, max_codes_cnt, charset = string.digits):
+        """ Generates and validates fail-proof codes. Each pair of generated codes has the Levenshtein distance 
+        equal or greater than min_dist parameter. Codes are resilient to floor(min_dist/2) typos.
+        Recommended settings: 
+        CodeGenerator(6,3,600) will generate codes resilient to 1 typo
+        CodeGenerator(10,5,1000) will generate codes resilient to 2 typos
+        CodeGenerator(14,7,4000)  will generate codes resilient to 3 typos
+        Parameters:
+        code_len (int): Length of the code to generate
+        min_dist (int):
+        max_codes_cnt (int): number of codes to generate
+        charset (str): charset used to generate codes, i.e. string.digits or string.ascii_uppercase
+
+        Returns:
+        CodeGenerator:Returning value
+        """
         if min_dist<3:
             raise Exception('Minimum distance should be greater than 2')
         if min_dist>=code_len:
@@ -14,26 +47,24 @@ class CodeGenerator():
         self.code_len = code_len
         self.min_dist = min_dist
         self.max_codes_cnt = max_codes_cnt
-        self.chars_digits = "1234567890"
-        self.chars_uppercase_letters = "ABCDEFGHIJKLMNOPQRTUVWXYZ"
+        self.sensitivity = floor(float(self.min_dist)/2)
 
         for i in range(0,self.max_codes_cnt):
-            code = self.gen_code()
+            code = self.gen_code(charset)
             self.codes.append(code)
 
-    def check_dist(self, new_code, min_dist):
+    def check_dist(self, new_code, min_dist, codes):
         matches = []
-        for code in self.codes:
+        for code in codes:
             l_dist = l.distance(new_code, code)
             if l_dist < min_dist:
                 matches.append({'code': code,'new_code': new_code, 'l_dist': l_dist})
         return matches
     
-    def gen_code(self):
+    def gen_code(self, charset):
         for i in range(0,1001):
-            # new_code = str(self.random_with_N_digits(self.code_len))
-            new_code = self.random_with_N_chars(self.code_len, self.chars_uppercase_letters)
-            if len(self.check_dist(new_code, self.min_dist)) > 0:
+            new_code = self.random_with_N_chars(self.code_len, charset)
+            if len(self.check_dist(new_code, self.min_dist, self.codes)) > 0:
                 continue
             else:
                 return new_code
@@ -42,15 +73,9 @@ class CodeGenerator():
     def check_code(self, input_code, sensitivity):
         if input_code in self.codes:
             return [input_code]
-        matches = self.check_dist(input_code, self.min_dist-sensitivity)
+        matches = self.check_dist(input_code, self.min_dist-sensitivity, self.codes)
         return [match['code'] for match in matches]
         
-    @staticmethod
-    def random_with_N_digits(n):
-        range_start = 10**(n-1)
-        range_end = (10**n)-1
-        return randint(range_start, range_end)
-
     @staticmethod
     def random_with_N_chars(n, char_table):
         result = ''
@@ -73,28 +98,34 @@ class TestUnitTest(unittest.TestCase):
     def test_no_typo(self):
         cg = CodeGenerator(6,3,600)
         for code in cg.codes:
-            check_result = cg.check_code(code, 1)
+            check_result = cg.check_code(code, cg.sensitivity)
             self.assertEqual([code],check_result)
 
     def test_one_char_typo(self):
         cg = CodeGenerator(6,3,600)
         for code in cg.codes:
             code_with_error = make_mistakes(code, 1)
-            check_result = cg.check_code(code_with_error, 1)
+            check_result = cg.check_code(code_with_error, cg.sensitivity)
             self.assertEqual([code],check_result)
+
+    def test_wrong_code(self):
+        cg = CodeGenerator(6,3,600)
+        wrong_code = "(*&(*&(^*())))"
+        check_result = cg.check_code(wrong_code, cg.sensitivity)
+        self.assertEqual([],check_result)
 
     def test_two_char_typo(self):
         cg = CodeGenerator(10,5,1000)
         for code in cg.codes:
             code_with_error = make_mistakes(code, 2)
-            check_result = cg.check_code(code_with_error, 2)
+            check_result = cg.check_code(code_with_error, cg.sensitivity)
             self.assertEqual([code],check_result)
                 
     def test_three_char_typo(self):
-        cg = CodeGenerator(14,7,6000)
+        cg = CodeGenerator(14,7,4000)
         for code in cg.codes:
             code_with_error = make_mistakes(code, 3)
-            check_result = cg.check_code(code_with_error, 3)
+            check_result = cg.check_code(code_with_error, cg.sensitivity)
             self.assertEqual([code],check_result)
 
 if __name__ == "__main__":
